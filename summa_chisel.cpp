b35b90489fbf37e2863f1a5306155609e5032996
vouching <v_ned@mail.ru>
2018-10-10 00:24:43 +0300

    the first task of my assignment

---
+++ summa_chisel.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<"summa chisel a i b ravna "<<a+b;
+}
