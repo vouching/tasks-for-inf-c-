139127852d2318eb74e535060af2e76930822763
vouching <v_ned@mail.ru>
2018-10-11 00:18:21 +0300

    complete task 3

--- vvod_ctroki.cpp
+++ vvod_ctroki.cpp
@@ -7,30 +7,31 @@
 using namespace std;
 int main()
 {
-	vector <char> as(93);
-	int i,maxq,k1,k2,k3;
-	string a;
+	vector <char> as(94);
+	int i,g,l1,l2,l3,maxq;
 	char s;
-	cin>>a;
-	k1=0;
-	k2=0;
-	k3=0;
-	i=0;
-	maxq=0;
+	i=1;
+	l1=0;
+	l2=0;
+	l3=0;
+	g=1;
 	while(i<4)
 	{
 		s=getchar();
-		if (s==' ')
-		{i++;if(i==4) break;}
-		else
-		{
-			if(i==1){k1++;}
-			if(i==2){k2++;}
-			if(i==3){k3++;}
-		}
+		as[g]=s;
+		if(s==' ') {i++;}
+		g++;
+			
 	}
-	maxq=max(k1,k2);
-	maxq=max(maxq,k3);
+	//for (int f=1;f<g;f++){cout<<as[f]<<", ";}
+	g=1;
+	while(as[g]!=' '){l1++;g++;}
+	g++;
+	while(as[g]!=' '){l2++;g++;}
+	g++;
+	while(as[g]!=' '){l3++;g++;}
+	maxq=max(l1,l2);
+	maxq=max(maxq,l3);
 	cout<<maxq; 
 }
 
