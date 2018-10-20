9e9726108f7e8a6414946b5b1f9fc2d0ac93f3d1
vouching <v_ned@mail.ru>
2018-10-21 00:14:12 +0300

    changes

--- tasks2/factorial.cpp
+++ tasks2/factorial.cpp
@@ -4,10 +4,9 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int main()
+int fac(int& a)
 {
-    int a;
-    cin>>a;
+    
     int b,c;
     b=2;
     c=1;
@@ -18,10 +17,18 @@ int main()
     }
     if(a>0)
     {
-    cout<<c;
+    a=c;
 	}
 	else
 	{
-	cout<<1;		
-	}
+	a=1;
+    }
+    return a;
+}
+int main()
+{
+    int f;
+    cin>>f;
+    f=fac(f);
+    cout<<f<<endl;
 }
