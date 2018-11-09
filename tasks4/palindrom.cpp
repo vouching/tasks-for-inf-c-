e24536e8cce6d851c41d39478553018a53062ece
vouching <v_ned@mail.ru>
2018-11-09 23:12:18 +0300

    dodelal

--- tasks4/palindrom.cpp
+++ tasks4/palindrom.cpp
@@ -17,59 +17,36 @@ void pr(const vector <char>& as)
 	}
 }
 
+void func(string a)
+{
+bool cond=0;
+unsigned int i=0;
+while(i<a.size())
+{
+if(a[i]==a[a.size()-1-i])
+cond=1;
+i++;
+}
+if(cond==1)
+cout<<a;
+else 
+cout<<"ne palindrom";
+}
 int main()
 {
-	bool cond=0;
-	unsigned int a;
-	
-	ifstream in ("gg.txt");
-	string d;
-	getline(in,d);
-	cout<<d[4]<<endl;
-	cout<<d<<endl;
+string a;
+cin>>a;
+cout<<a<<endl;
+func(a);
+vector <string> as(100);
+unsigned int i=0;
+while(i<as.size())
+{
 	cin>>a;
-	unsigned int counter=0;
-	vector <char> pal;
-	vector <char> as;
-	unsigned int i=0;
-	while(i<d.size())
-	{
-		as.push_back(0);
-		
-		as[counter]=d[i];
-		
-		counter++;
-		if (d[i]==' ')
-		{
-			unsigned int g=0;
-			while (g<i)
-			{
-				
-				if((as[g]==as[i-1-g]))
-				{cond=1;}
-				g++;
-			}
-			if(cond==1)
-			{
-				unsigned int j=0;
-				while(j<counter)
-				{
-					
-					pal.push_back(as[j]);
-					
-					j++;
-					
-				}
-				as.clear();
-				counter=0;
-			}
-		//cout<<cond<<endl;
-		}
-		
-		i++;
-		if(cond==1){cond=0;}
-	}
 	
-	pr(pal);
+	as[i]=a;
+	func(a);
+	i++;
 }
 
+}
